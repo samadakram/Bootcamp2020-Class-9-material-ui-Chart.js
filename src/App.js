f25b3f4638265bc6d1c4241e2c0d6d9d7a3f4cde
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-25 04:42:55 +0500

    Created Ui Login Form
    created Ui of Login form by using Material-ui components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { Paper, TextField, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: 50,
+  },
+  paper: {
+    width: 400,
+    margin: '0 auto',
+    textAlign: 'center',
+    padding: 15,
+  },
+  input: {
+    width: '95%',
+    marginBottom: 10,
+  }
+}));
 
 function App() {
+
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      Hello World From Abdul Samad !
+    <div className={classes.root}>
+      <Paper elevation={3} className={classes.paper}>
+        <h1> Login </h1>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField label="Enter Email" className={classes.input} />
+          <TextField label="Enter Password" className={classes.input} />
+          <Button variant="contained" color="primary" className={classes.input}>
+            Primary
+          </Button>
+        </form>
+      </Paper>
     </div>
   );
 }
