3b123fde59ad34412c553dbbfe83a275060cb43d
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-25 19:12:34 +0500

    Updated BarChart
    modified Ui of chart

--- src/BarChart.js
+++ src/BarChart.js
@@ -9,9 +9,18 @@ const data = {
             backgroundColor: 'rgba(255,99,132,0.2)',
             borderColor: 'rgba(255,99,132,1)',
             borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+            hoverBackgroundColor: 'yellow',
             hoverBorderColor: 'rgba(255,99,132,1)',
             data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: 'My Second dataset',
+            backgroundColor: 'rgba(25,85,80,0.2)',
+            borderColor: 'rgba(25,85,80,1)',
+            borderWidth: 1,
+            hoverBackgroundColor: 'red',
+            hoverBorderColor: 'rgba(255,99,132,1)',
+            data: [50, 60, 85, 91, 100, 25, 30]
         }
     ]
 };
